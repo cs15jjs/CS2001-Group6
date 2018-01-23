8c79e04ee95eac8df1c1d678fd9633b53252bb3f
Jo-Neil Starkie 1422088 <1422088@brunel.ac.uk>
2018-01-23 18:25:26 +0000

    Android practise

---
+++ PullDataListViewTest/app/src/main/java/joneilstarkie/pulldatalistviewtest/JSONParser.java
@@ -0,0 +1,26 @@
+package joneilstarkie.pulldatalistviewtest;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class JSONParser extends AsyncTask<Void, Void, String>
+{
+
+    @Override
+    protected String doInBackground(Void... voids) {
+        return null;
+    }
+
+    public static void requestMovie()
+    {
+
+    }
+}
---
+++ PullDataListViewTest/app/src/main/java/joneilstarkie/pulldatalistviewtest/MainActivity.java
@@ -0,0 +1,32 @@
+package joneilstarkie.pulldatalistviewtest;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        final Context context = this;
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent intent = new Intent(context, JSONParser.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
